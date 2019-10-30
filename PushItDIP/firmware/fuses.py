605b695bd4cf90d12189024aaed384471739a3cb
Thomas Basler <github.thomas@familie-basler.net>
2019-10-30 19:49:56 +0100

    PushItDIP: Erase when writing fuses

--- PushItDIP/firmware/fuses.py
+++ PushItDIP/firmware/fuses.py
@@ -1,2 +1,2 @@
 Import('env')
-env.Replace(FUSESCMD="$UPLOADER $UPLOADERFLAGS -Ulock:w:0xFF:m -Uhfuse:w:0xD9:m -Uefuse:w:0xFD:m -Ulfuse:w:0xFF:m")
+env.Replace(FUSESCMD="$UPLOADER $UPLOADERFLAGS -e -Ulock:w:0xFF:m -Uhfuse:w:0xD9:m -Uefuse:w:0xFD:m -Ulfuse:w:0xFF:m")
